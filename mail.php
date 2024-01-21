ac7bb11660993ab963449378f0a408ec46a06b44
Bolat Abilayev <babilayev@Bolats-MacBook-Pro.local>
2024-01-21 20:48:44 +0600

    landing page init

---
+++ mail.php
@@ -0,0 +1,45 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'utf-8';
+
+    $name = $_POST['name'];
+    $phone= $_POST['phone'];
+
+    try{
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = 'mail.akkadapay.kz';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'info@akkadapay.kz';
+        $mail->Password = 'Qr2023@eQ';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('info@akkadapay.kz');
+        $mail->addAddress('bula86kz@gmail.com');
+        $mail->isHTML(true);
+
+        $mail->Subject = "AKKADAPAY: Заявка с сайта";
+        $mail->Body = 'Пользователь: ' .$name . 'оставил заявку. Контакты: ' .$phone;
+        $mail->AltBody = 'Пользователь: ' .$name .' оставил заявку. Контакты: ' .$phone;
+  
+        $mail->send();
+
+        if(!$mail->send()) {
+            echo 'Error';
+        } else {
+            header('location: thankyou.html');
+        }
+    } catch(Exception $e){
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+
+
+?>
